40e64407e3933943250768f10c63da0a780bc9c3
Simon Boylan <siboy73@gmail.com>
2023-11-27 14:49:58 +0000

    Add function to  get weather by city input

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function getWeather() {
+  const apiKey = 'b22d08d2f4337aadcbe469786fbe812e';
+  const cityInput = document.getElementById('cityInput');
+  const cityName = cityInput.value;
+
+  if (cityName === '') {
+    alert('Please enter a city name.');
+    return;
+}
+
+
+}
