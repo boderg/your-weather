83f8a8549f32a13c451f97da5112e0be8e6956af
Simon Boylan <siboy73@gmail.com>
2023-12-01 21:46:23 +0000

    Add function to make modal operational

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,23 @@ document.getElementById('form').addEventListener('submit', function(event) {
   getWeather();
 })
 }
+
+var modal = document.getElementById("about");
+
+var btn = document.getElementById("aboutBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
