6ecabb744e647bb45a959017ed816d172943a01e
Simon Boylan <siboy73@gmail.com>
2023-12-02 15:19:55 +0000

    Add comments to code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,97 @@
+// Declare units variable to store units of measurement, default to metric.
 let units = 'metric';
 
+/* Function to get weather data from OpenWeatherMap API, 
+ *  declaring api key and city name based on user input.
+ */
 function getWeather() {
   const apiKey = 'b22d08d2f4337aadcbe469786fbe812e';
   const cityInput = document.getElementById('cityInput');
   const cityName = cityInput.value;
 
+  // If city name is empty, alert user to enter a city name and return.
   if (cityName === '') {
     alert('Please enter a city name.');
     return;
   }
 
+  //  Declaring geolocation url based on city name and api key.
   const geolocationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
 
+  // Fetching geolocation data from OpenWeatherMap API based on city name and api key.
   fetch(geolocationUrl)
     .then(response => response.json())
     .then(data => {
       const lat = data[0].lat;
       const lon = data[0].lon;
 
-  const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=${units}`;
+      // Declaring weather url based on latitude, longitude, api key and units of measurement.
+      const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=${units}`;
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      displayWeather(data);
-      displayForecast(data);
+      /* Fetching weather data from OpenWeatherMap API based on 
+       *  latitude, longitude, api key and units of measurement.
+       */
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          displayWeather(data);
+          displayForecast(data);
+        })
+
+        // Catching errors and alerting user to try again.
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+          alert('Error fetching weather data. Please try again.');
+        });
     })
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-      alert('Error fetching weather data. Please try again.');
-    });
-})
 
-function displayWeather(data) {
-  const weatherInfo = document.getElementById('weatherInfo');
-  const temperature = data.current.temp;
-  const feels_like = data.current.feels_like;
-  const icon = data.current.weather[0].icon;
-  const description = data.current.weather[0].description;
-  const humidity = data.current.humidity;
-  const wind = data.current.wind_speed;
-  const pressure = data.current.pressure;
-  const sunrise  = data.current.sunrise;
-  const sunset = data.current.sunset;
-  const timezoneOffset = data.timezone_offset;
+  // Function to declare variables based on weather data.
+  function displayWeather(data) {
+    const weatherInfo = document.getElementById('weatherInfo');
+    const temperature = data.current.temp;
+    const feels_like = data.current.feels_like;
+    const icon = data.current.weather[0].icon;
+    const description = data.current.weather[0].description;
+    const humidity = data.current.humidity;
+    const wind = data.current.wind_speed;
+    const pressure = data.current.pressure;
+    const sunrise = data.current.sunrise;
+    const sunset = data.current.sunset;
+    const timezoneOffset = data.timezone_offset;
 
-  const now = new Date();
-  const cityTime = new Date(now.getTime() + (now.getTimezoneOffset() * 60 * 1000) + (timezoneOffset * 1000));
-  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-  const cityDateTime = new Intl.DateTimeFormat('en-GB', options).format(cityTime);
+    /* Timezone offset is added to current time to 
+     *  get local time based on user input.
+     */
+    const now = new Date();
+    const cityTime = new Date(now.getTime() + (now.getTimezoneOffset() * 60 * 1000) + (timezoneOffset * 1000));
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    };
+    const cityDateTime = new Intl.DateTimeFormat('en-GB', options).format(cityTime);
 
-  const sunriseDate = new Date((sunrise + timezoneOffset) * 1000);
-  const sunsetDate = new Date((sunset + timezoneOffset) * 1000);
+    /* Sunrise and sunset times are converted from unix timestamp 
+     *  to local time based on user input.  
+     */
+    const sunriseDate = new Date((sunrise + timezoneOffset) * 1000);
+    const sunsetDate = new Date((sunset + timezoneOffset) * 1000);
 
-  const sunriseTime = sunriseDate.getUTCHours().toString().padStart(2, '0') + ':' + sunriseDate.getUTCMinutes().toString().padStart(2, '0');
-  const sunsetTime = sunsetDate.getUTCHours().toString().padStart(2, '0') + ':' + sunsetDate.getUTCMinutes().toString().padStart(2, '0');
+    // Sunrise and sunset times are formatted to 24 hour clock.
+    const sunriseTime = sunriseDate.getUTCHours().toString().padStart(2, '0') + ':' + sunriseDate.getUTCMinutes().toString().padStart(2, '0');
+    const sunsetTime = sunsetDate.getUTCHours().toString().padStart(2, '0') + ':' + sunsetDate.getUTCMinutes().toString().padStart(2, '0');
 
-  let tempSymbol = units === 'imperial' ? '°f' : '°c';
-  let windUnit = units === 'imperial' ? 'mph' : 'm/s';
+    /* Temperature, feels like temperature and wind speed 
+     *  are queried based on units of measurement.
+     */
+    let tempSymbol = units === 'imperial' ? '°f' : '°c';
+    let windUnit = units === 'imperial' ? 'mph' : 'm/s';
 
-  const htmlCurrent = `
+    // HTML is generated based on weather data.
+    const htmlCurrent = `
                 <p class="date-time">${cityDateTime}</p>
                 <h2 class="city">${cityName}</h2>
                 <h1 class="temp">${temperature.toFixed(1)} <sup>${tempSymbol}</sup></h1>
@@ -71,68 +103,82 @@ function displayWeather(data) {
                 <p class="pressure">Atmospheric Pressure: ${pressure} hPa</p>
                 <p class="sunrise-set">Sunrise: ${sunriseTime} / Sunset: ${sunsetTime}</p>`;
 
-  weatherInfo.innerHTML = htmlCurrent;
-}
+    // HTML is displayed on page.
+    weatherInfo.innerHTML = htmlCurrent;
+  }
 
-function displayForecast(data) {
-  const forecastInfo = document.getElementById('forecastInfo');
-  const forecast = data.daily;
+  // Function to declare variables based on forecast data.
+  function displayForecast(data) {
+    const forecastInfo = document.getElementById('forecastInfo');
+    const forecast = data.daily;
 
-  let htmlForecast = '';
+    // htmlForecast variable is declared as an empty string.
+    let htmlForecast = '';
 
-  for (let i = 1; i <= 4; i++) {
-    const forecastDate = new Date((forecast[i].dt + data.timezone_offset) * 1000);
-    const forecastDay = forecastDate.toLocaleString('en-GB', { weekday: 'long' });
-    const forecastIcon = forecast[i].weather[0].icon;
-    const forecastTemp = forecast[i].temp.day.toFixed(1);
-    const forecastDescription = forecast[i].weather[0].description;
+    /* For loop is used to iterate through forecast data
+     * and declare variables based on that data.
+     */
+    for (let i = 1; i <= 4; i++) {
+      const forecastDate = new Date((forecast[i].dt + data.timezone_offset) * 1000);
+      const forecastDay = forecastDate.toLocaleString('en-GB', {
+        weekday: 'long'
+      });
+      const forecastIcon = forecast[i].weather[0].icon;
+      const forecastTemp = forecast[i].temp.day.toFixed(1);
+      const forecastDescription = forecast[i].weather[0].description;
 
-    let tempSymbol = units === 'imperial' ? '°f' : '°c';
+      // Temperature is queried based on units of measurement.
+      let tempSymbol = units === 'imperial' ? '°f' : '°c';
 
-    htmlForecast += `
+      // HTML is generated based on forecast data.
+      htmlForecast += `
                 <div class="forecast-day">
                     <p class="forecast-date">${forecastDay}</p>
                     <p class="forecast-temp">${forecastTemp} <sup>${tempSymbol}</sup></p>
                     <img class="forecast-icon" src="https://openweathermap.org/img/wn/${forecastIcon}.png" alt="forecastIcon">                    
                     <p class="forecast-description">${forecastDescription}</p>
                 </div>`;
+    }
+
+    // HTML is displayed on page.  
+    forecastInfo.innerHTML = htmlForecast;
+
   }
 
-  forecastInfo.innerHTML = htmlForecast;
-
+  document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    getWeather();
+  })
 }
 
-document.getElementById('form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  getWeather();
-})
-}
-
-document.getElementById('units').addEventListener('change', function() {
+// Event listener to change units of measurement based on user input.
+document.getElementById('units').addEventListener('change', function () {
   if (this.checked) {
-      units = 'imperial';
+    units = 'imperial';
   } else {
-      units = 'metric';
+    units = 'metric';
   }
   getWeather();
 });
 
+// Declare variables for modal
 var modal = document.getElementById("about");
-
 var btn = document.getElementById("aboutBtn");
-
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+// Event listeners to open modal
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-span.onclick = function() {
+// Event listener to close modal when user clicks close 'x'
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+// Event listener to close modal when user clicks outside of modal
+window.onclick = function (event) {
   if (event.target == modal) {
-      modal.style.display = "none";
+    modal.style.display = "none";
   }
 }
